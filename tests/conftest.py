10dab2abaa5830881c83458db51a57a71bdd0573
OzkanSisik <ozkansisik@gmail.com>
2025-07-27 21:22:56 +0300

    Fix conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from api.pokemon_api import PokemonAPI
-from utils import get_base_url
+from config.settings import BASE_URL
 
 
 @pytest.fixture(scope="session")
@@ -10,4 +10,4 @@ def pokemon_api():
 
 @pytest.fixture(scope="session")
 def api_base_url():
-    return get_base_url()
+    return BASE_URL
